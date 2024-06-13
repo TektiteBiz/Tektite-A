a1b7881b8e73ab2ac1211988676713f23742a75b
Nv7-Github <junk4Nv7@gmail.com>
2024-06-12 17:29:54 -0700

    Fix bmi088 code

--- Core/Src/bmi088.c
+++ Core/Src/bmi088.c
@@ -6,16 +6,17 @@
  *
  */
 uint8_t BMI088_Init(BMI088 *imu,
-				 SPI_HandleTypeDef *spiHandle,
-				 GPIO_TypeDef *csAccPinBank, uint16_t csAccPin,
-				 GPIO_TypeDef *csGyrPinBank, uint16_t csGyrPin) {
+					SPI_HandleTypeDef *spiHandle,
+					GPIO_TypeDef *csAccPinBank, uint16_t csAccPin,
+					GPIO_TypeDef *csGyrPinBank, uint16_t csGyrPin)
+{
 
 	/* Store interface parameters in struct */
-	imu->spiHandle 		= spiHandle;
-	imu->csAccPinBank 	= csAccPinBank;
-	imu->csAccPin 		= csAccPin;
-	imu->csGyrPinBank 	= csGyrPinBank;
-	imu->csGyrPin 		= csGyrPin;
+	imu->spiHandle = spiHandle;
+	imu->csAccPinBank = csAccPinBank;
+	imu->csAccPin = csAccPin;
+	imu->csGyrPinBank = csGyrPinBank;
+	imu->csGyrPin = csGyrPin;
 
 	/* Clear DMA flags */
 	imu->readingAcc = 0;
@@ -43,10 +44,10 @@ uint8_t BMI088_Init(BMI088 *imu,
 	uint8_t chipID;
 	status += BMI088_ReadAccRegister(imu, BMI_ACC_CHIP_ID, &chipID);
 
-	if (chipID != 0x1E) {
+	if (chipID != 0x1E)
+	{
 
-	//	return 0;
-
+		//	return 0;
 	}
 	HAL_Delay(10);
 
@@ -58,11 +59,11 @@ uint8_t BMI088_Init(BMI088 *imu,
 	HAL_Delay(10);
 
 	/* Enable accelerometer data ready interrupt */
-	//status += BMI088_WriteAccRegister(imu, BMI_INT1_IO_CONF, 0x0A); /* INT1 = push-pull output, active high */
-	//HAL_Delay(10);
+	// status += BMI088_WriteAccRegister(imu, BMI_INT1_IO_CONF, 0x0A); /* INT1 = push-pull output, active high */
+	// HAL_Delay(10);
 
-	//status += BMI088_WriteAccRegister(imu, BMI_INT1_INT2_MAP_DATA, 0x04);
-	//HAL_Delay(10);
+	// status += BMI088_WriteAccRegister(imu, BMI_INT1_INT2_MAP_DATA, 0x04);
+	// HAL_Delay(10);
 
 	/* Put accelerometer into active mode */
 	status += BMI088_WriteAccRegister(imu, BMI_ACC_PWR_CONF, 0x00);
@@ -73,8 +74,8 @@ uint8_t BMI088_Init(BMI088 *imu,
 	HAL_Delay(10);
 
 	/* Pre-compute accelerometer conversion constant (raw to m/s^2) */
-	imu->accConversion = 9.81f / 32768.0f * 16.0f * 1.5f; /* Datasheet page 27 */
-	
+	imu->accConversion = 1.0f / 32768.0f * 16.0f * 1.5f; /* Datasheet page 27 */
+
 	/* Set accelerometer TX buffer for DMA */
 	imu->accTxBuf[0] = BMI_ACC_DATA | 0x80;
 
@@ -93,10 +94,10 @@ uint8_t BMI088_Init(BMI088 *imu,
 	/* Check chip ID */
 	status += BMI088_ReadGyrRegister(imu, BMI_GYR_CHIP_ID, &chipID);
 
-	if (chipID != 0x0F) {
+	if (chipID != 0x0F)
+	{
 
-		//return 0;
-
+		// return 0;
 	}
 	HAL_Delay(10);
 
@@ -104,27 +105,26 @@ uint8_t BMI088_Init(BMI088 *imu,
 	status += BMI088_WriteGyrRegister(imu, BMI_GYR_RANGE, 0x00); /* +- 2000 deg/s */
 	HAL_Delay(10);
 
-	status += BMI088_WriteGyrRegister(imu, BMI_GYR_BANDWIDTH, 0x02); /* ODR = 400 Hz, Filter bandwidth = 116 Hz */
+	status += BMI088_WriteGyrRegister(imu, BMI_GYR_BANDWIDTH, 0x04); /* ODR = 200 Hz, Filter bandwidth = 23 Hz */
 	HAL_Delay(10);
 
 	/* Enable gyroscope data ready interrupt */
-	//status += BMI088_WriteGyrRegister(imu, BMI_GYR_INT_CTRL, 0x80); /* New data interrupt enabled */
-	//HAL_Delay(10);
+	// status += BMI088_WriteGyrRegister(imu, BMI_GYR_INT_CTRL, 0x80); /* New data interrupt enabled */
+	// HAL_Delay(10);
 
-	//status += BMI088_WriteGyrRegister(imu, BMI_INT3_INT4_IO_CONF, 0x01); /* INT3 = push-pull, active high */
-	//HAL_Delay(10);
+	// status += BMI088_WriteGyrRegister(imu, BMI_INT3_INT4_IO_CONF, 0x01); /* INT3 = push-pull, active high */
+	// HAL_Delay(10);
 
-	//status += BMI088_WriteGyrRegister(imu, BMI_INT3_INT4_IO_MAP, 0x01); /* Data ready interrupt mapped to INT3 pin */
-	//HAL_Delay(10);
+	// status += BMI088_WriteGyrRegister(imu, BMI_INT3_INT4_IO_MAP, 0x01); /* Data ready interrupt mapped to INT3 pin */
+	// HAL_Delay(10);
 
 	/* Pre-compute gyroscope conversion constant (raw to rad/s) */
 	imu->gyrConversion = 0.01745329251f * 1000.0f / 16384.0f; /* Datasheet page 39 */
-	
+
 	/* Set gyroscope TX buffer for DMA */
 	imu->gyrTxBuf[0] = BMI_GYR_DATA | 0x80;
 
 	return status;
-
 }
 
 /*
@@ -134,7 +134,8 @@ uint8_t BMI088_Init(BMI088 *imu,
  */
 
 /* ACCELEROMETER READS ARE DIFFERENT TO GYROSCOPE READS. SEND ONE BYTE ADDRESS, READ ONE DUMMY BYTE, READ TRUE DATA !!! */
-uint8_t BMI088_ReadAccRegister(BMI088 *imu, uint8_t regAddr, uint8_t *data) {
+uint8_t BMI088_ReadAccRegister(BMI088 *imu, uint8_t regAddr, uint8_t *data)
+{
 
 	uint8_t txBuf[3] = {regAddr | 0x80, 0x00, 0x00};
 	uint8_t rxBuf[3];
@@ -143,17 +144,17 @@ uint8_t BMI088_ReadAccRegister(BMI088 *imu, uint8_t regAddr, uint8_t *data) {
 	uint8_t status = (HAL_SPI_TransmitReceive(imu->spiHandle, txBuf, rxBuf, 3, HAL_MAX_DELAY) != HAL_OK);
 	HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_SET);
 
-	if (status == 1) {
+	if (status == 1)
+	{
 
 		*data = rxBuf[2];
-
 	}
 
 	return status;
-
 }
 
-uint8_t BMI088_ReadGyrRegister(BMI088 *imu, uint8_t regAddr, uint8_t *data) {
+uint8_t BMI088_ReadGyrRegister(BMI088 *imu, uint8_t regAddr, uint8_t *data)
+{
 
 	uint8_t txBuf[2] = {regAddr | 0x80, 0x00};
 	uint8_t rxBuf[2];
@@ -162,50 +163,50 @@ uint8_t BMI088_ReadGyrRegister(BMI088 *imu, uint8_t regAddr, uint8_t *data) {
 	uint8_t status = (HAL_SPI_TransmitReceive(imu->spiHandle, txBuf, rxBuf, 2, HAL_MAX_DELAY) != HAL_OK);
 	HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_SET);
 
-	if (status == 1) {
+	if (status == 1)
+	{
 
 		*data = rxBuf[1];
-
 	}
 
 	return status;
-
 }
 
-uint8_t BMI088_WriteAccRegister(BMI088 *imu, uint8_t regAddr, uint8_t data) {
+uint8_t BMI088_WriteAccRegister(BMI088 *imu, uint8_t regAddr, uint8_t data)
+{
 
 	uint8_t txBuf[2] = {regAddr, data};
 
 	HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_RESET);
 	uint8_t status = (HAL_SPI_Transmit(imu->spiHandle, txBuf, 2, HAL_MAX_DELAY) != HAL_OK);
-	while(HAL_SPI_GetState(imu->spiHandle) != HAL_SPI_STATE_READY);
+	while (HAL_SPI_GetState(imu->spiHandle) != HAL_SPI_STATE_READY)
+		;
 	HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_SET);
 
 	return status;
-
 }
 
-uint8_t BMI088_WriteGyrRegister(BMI088 *imu, uint8_t regAddr, uint8_t data) {
+uint8_t BMI088_WriteGyrRegister(BMI088 *imu, uint8_t regAddr, uint8_t data)
+{
 
 	uint8_t txBuf[2] = {regAddr, data};
 
 	HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_RESET);
 	uint8_t status = (HAL_SPI_Transmit(imu->spiHandle, txBuf, 2, HAL_MAX_DELAY) != HAL_OK);
-	while(HAL_SPI_GetState(imu->spiHandle) != HAL_SPI_STATE_READY);
+	while (HAL_SPI_GetState(imu->spiHandle) != HAL_SPI_STATE_READY)
+		;
 	HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_SET);
 
 	return status;
-
 }
-
-
 
 /*
  *
  * POLLING
  *
  */
-uint8_t BMI088_ReadAccelerometer(BMI088 *imu) {
+uint8_t BMI088_ReadAccelerometer(BMI088 *imu)
+{
 
 	/* Read raw accelerometer data */
 	uint8_t txBuf[8] = {(BMI_ACC_DATA | 0x80), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; /* Register addr, 1 byte dummy, 6 bytes data */
@@ -216,9 +217,9 @@ uint8_t BMI088_ReadAccelerometer(BMI088 *imu) {
 	HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_SET);
 
 	/* Form signed 16-bit integers */
-	int16_t accX = (int16_t) ((rxBuf[3] << 8) | rxBuf[2]);
-	int16_t accY = (int16_t) ((rxBuf[5] << 8) | rxBuf[4]);
-	int16_t accZ = (int16_t) ((rxBuf[7] << 8) | rxBuf[6]);
+	int16_t accX = (int16_t)((rxBuf[3] << 8) | rxBuf[2]);
+	int16_t accY = (int16_t)((rxBuf[5] << 8) | rxBuf[4]);
+	int16_t accZ = (int16_t)((rxBuf[7] << 8) | rxBuf[6]);
 
 	/* Convert to m/s^2 */
 	imu->acc_mps2[0] = imu->accConversion * accX;
@@ -226,10 +227,10 @@ uint8_t BMI088_ReadAccelerometer(BMI088 *imu) {
 	imu->acc_mps2[2] = imu->accConversion * accZ;
 
 	return status;
-
 }
 
-uint8_t BMI088_ReadGyroscope(BMI088 *imu) {
+uint8_t BMI088_ReadGyroscope(BMI088 *imu)
+{
 
 	/* Read raw gyroscope data */
 	uint8_t txBuf[7] = {(BMI_GYR_DATA | 0x80), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; /* Register addr, 6 bytes data */
@@ -240,9 +241,9 @@ uint8_t BMI088_ReadGyroscope(BMI088 *imu) {
 	HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_SET);
 
 	/* Form signed 16-bit integers */
-	int16_t gyrX = (int16_t) ((rxBuf[1] << 8) | rxBuf[2]);
-	int16_t gyrY = (int16_t) ((rxBuf[3] << 8) | rxBuf[4]);
-	int16_t gyrZ = (int16_t) ((rxBuf[5] << 8) | rxBuf[6]);
+	int16_t gyrX = (int16_t)((rxBuf[2] << 8) | rxBuf[1]);
+	int16_t gyrY = (int16_t)((rxBuf[4] << 8) | rxBuf[3]);
+	int16_t gyrZ = (int16_t)((rxBuf[6] << 8) | rxBuf[5]);
 
 	/* Convert to rad/s */
 	imu->gyr_rps[0] = imu->gyrConversion * gyrX;
@@ -250,7 +251,6 @@ uint8_t BMI088_ReadGyroscope(BMI088 *imu) {
 	imu->gyr_rps[2] = imu->gyrConversion * gyrZ;
 
 	return status;
-
 }
 
 /*
@@ -258,70 +258,72 @@ uint8_t BMI088_ReadGyroscope(BMI088 *imu) {
  * DMA
  *
  */
-uint8_t BMI088_ReadAccelerometerDMA(BMI088 *imu) {
+uint8_t BMI088_ReadAccelerometerDMA(BMI088 *imu)
+{
 
 	HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_RESET);
-	if (HAL_SPI_TransmitReceive_DMA(imu->spiHandle, imu->accTxBuf, (uint8_t *) imu->accRxBuf, 8) != HAL_OK) {
+	if (HAL_SPI_TransmitReceive_DMA(imu->spiHandle, imu->accTxBuf, (uint8_t *)imu->accRxBuf, 8) != HAL_OK)
+	{
 
 		imu->readingAcc = 1;
 		return 1;
-
-	} else {
+	}
+	else
+	{
 
 		HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_SET);
 		return 0;
-
 	}
-
 }
 
-void BMI088_ReadAccelerometerDMA_Complete(BMI088 *imu) {
+void BMI088_ReadAccelerometerDMA_Complete(BMI088 *imu)
+{
 
 	HAL_GPIO_WritePin(imu->csAccPinBank, imu->csAccPin, GPIO_PIN_SET);
 	imu->readingAcc = 0;
 
 	/* Form signed 16-bit integers */
-	int16_t accX = (int16_t) ((imu->accRxBuf[3] << 8) | imu->accRxBuf[2]);
-	int16_t accY = (int16_t) ((imu->accRxBuf[5] << 8) | imu->accRxBuf[4]);
-	int16_t accZ = (int16_t) ((imu->accRxBuf[7] << 8) | imu->accRxBuf[6]);
+	int16_t accX = (int16_t)((imu->accRxBuf[3] << 8) | imu->accRxBuf[2]);
+	int16_t accY = (int16_t)((imu->accRxBuf[5] << 8) | imu->accRxBuf[4]);
+	int16_t accZ = (int16_t)((imu->accRxBuf[7] << 8) | imu->accRxBuf[6]);
 
 	/* Convert to m/s^2 */
 	imu->acc_mps2[0] = imu->accConversion * accX;
 	imu->acc_mps2[1] = imu->accConversion * accY;
 	imu->acc_mps2[2] = imu->accConversion * accZ;
-
 }
 
-uint8_t BMI088_ReadGyroscopeDMA(BMI088 *imu) {
+uint8_t BMI088_ReadGyroscopeDMA(BMI088 *imu)
+{
 
 	HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_RESET);
-	if (HAL_SPI_TransmitReceive_DMA(imu->spiHandle, imu->gyrTxBuf, (uint8_t *) imu->gyrRxBuf, 7) != HAL_OK) {
+	if (HAL_SPI_TransmitReceive_DMA(imu->spiHandle, imu->gyrTxBuf, (uint8_t *)imu->gyrRxBuf, 7) != HAL_OK)
+	{
 
 		imu->readingGyr = 1;
 		return 1;
-
-	} else {
+	}
+	else
+	{
 
 		HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_SET);
 		return 0;
-
 	}
-
 }
 
-void BMI088_ReadGyroscopeDMA_Complete(BMI088 *imu) {
+void BMI088_ReadGyroscopeDMA_Complete(BMI088 *imu)
+{
 
 	HAL_GPIO_WritePin(imu->csGyrPinBank, imu->csGyrPin, GPIO_PIN_SET);
 	imu->readingGyr = 0;
 
 	/* Form signed 16-bit integers */
-	int16_t gyrX = (int16_t) ((imu->gyrRxBuf[2] << 8) | imu->gyrRxBuf[1]);
-	int16_t gyrY = (int16_t) ((imu->gyrRxBuf[4] << 8) | imu->gyrRxBuf[3]);
-	int16_t gyrZ = (int16_t) ((imu->gyrRxBuf[6] << 8) | imu->gyrRxBuf[5]);
+	int16_t gyrX = (int16_t)((imu->gyrRxBuf[2] << 8) | imu->gyrRxBuf[1]);
+	int16_t gyrY = (int16_t)((imu->gyrRxBuf[4] << 8) | imu->gyrRxBuf[3]);
+	int16_t gyrZ = (int16_t)((imu->gyrRxBuf[6] << 8) | imu->gyrRxBuf[5]);
 
 	/* Convert to deg/s */
 	imu->gyr_rps[0] = imu->gyrConversion * gyrX;
 	imu->gyr_rps[1] = imu->gyrConversion * gyrY;
 	imu->gyr_rps[2] = imu->gyrConversion * gyrZ;
-
 }
